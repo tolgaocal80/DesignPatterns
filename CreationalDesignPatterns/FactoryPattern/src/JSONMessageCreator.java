0ca2f0b026f906697f446a8707da0866210d3bf8
tolgaocal80 <tolgaocal80@gmail.com>
2022-04-24 13:46:11 +0300

    initial

---
+++ CreationalDesignPatterns/FactoryPattern/src/JSONMessageCreator.java
@@ -0,0 +1,12 @@
+import message.JSONMessage;
+import message.Message;
+
+public class JSONMessageCreator extends MessageCreator{
+
+    @Override
+    public Message createMessage() {
+        return new JSONMessage();
+    }
+
+
+}
---
+++ CreationalDesignPatterns/FactoryPattern/src/TextMessageCreator.java
@@ -0,0 +1,11 @@
+import message.Message;
+import message.TextMessage;
+
+public class TextMessageCreator extends MessageCreator{
+    @Override
+    public Message createMessage() {
+        return new TextMessage();
+    }
+
+
+}
